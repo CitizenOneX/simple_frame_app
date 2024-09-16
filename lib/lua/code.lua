78c2a7d86f8c8d47545308b50c4e6d4b862fbd6c
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-09-16 16:03:54 +1000

    added frameside TxCode message parser

---
+++ lib/lua/code.lua
@@ -0,0 +1,11 @@
+-- Module to parse message codes sent from phoneside app as TxCode messages
+_M = {}
+
+-- Parse the TxCode message raw data, which is a single byte
+function _M.parse_code(data)
+	local code = {}
+	code.value = string.byte(data, 1)
+	return code
+end
+
+return _M
