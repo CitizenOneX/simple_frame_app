066bc2a61059ead435afe7c5cd9968df8b8aaf61
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-09-16 15:31:43 +1000

    added single byte to txcode payload

--- lib/tx/code.dart
+++ lib/tx/code.dart
@@ -2,12 +2,16 @@ import 'dart:typed_data';
 
 import 'package:simple_frame_app/tx_msg.dart';
 
-/// A message containing only the msgCode and no additional data
+/// A message containing only the msgCode and a single optional byte
 /// suitable for signalling the frameside app to take some action
 /// (e.g. toggle streaming, take a photo with default parameters etc.)
 class TxCode extends TxMsg {
-  TxCode({required super.msgCode});
+  int value;
+
+  TxCode({required super.msgCode, this.value = 0});
 
   @override
-  Uint8List pack() => Uint8List(0);
+  Uint8List pack() {
+     return Uint8List.fromList([value & 0xFF]);
+  }
 }
