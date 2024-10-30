4e24f5160f15fa792f19b6a2cee641f266868830
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-10-31 09:05:36 +1100

    updated camera.lua and tx/camera_settings.dart to support both autoexposure and manual exposure photos

--- lib/lua/camera.lua
+++ lib/lua/camera.lua
@@ -12,32 +12,60 @@ function _M.parse_camera_settings(data)
 
 	local camera_settings = {}
 	-- quality and metering mode are indices into arrays of values (0-based phoneside; 1-based in Lua)
-	-- exposure maps from 0..255 to -2.0..+2.0
 	camera_settings.quality = quality_values[string.byte(data, 1) + 1]
-	camera_settings.auto_exp_gain_times = string.byte(data, 2)
-	camera_settings.metering = metering_values[string.byte(data, 3) + 1]
-	camera_settings.exposure = string.byte(data, 4) / 255.0
-	camera_settings.exposure_speed = string.byte(data, 5) / 255.0
-	camera_settings.shutter_limit = string.byte(data, 6) << 8 | string.byte(data, 7)
-	camera_settings.analog_gain_limit = string.byte(data, 8)
-	camera_settings.white_balance_speed = string.byte(data, 9) / 255.0
+	camera_settings.auto = string.byte(data, 2) > 0
+
+	if camera_settings.auto == true then
+		camera_settings.auto_exp_gain_times = string.byte(data, 2)
+		camera_settings.auto_exp_interval = string.byte(data, 3) / 1000.0
+		camera_settings.metering = metering_values[string.byte(data, 4) + 1]
+		camera_settings.exposure = string.byte(data, 5) / 255.0
+		camera_settings.exposure_speed = string.byte(data, 6) / 255.0
+		camera_settings.shutter_limit = string.byte(data, 7) << 8 | string.byte(data, 8) & 0x3FFF
+		camera_settings.analog_gain_limit = string.byte(data, 9) & 0xF8
+		camera_settings.white_balance_speed = string.byte(data, 10) / 255.0
+	else
+		camera_settings.manual_shutter = string.byte(data, 11) << 8 | string.byte(data, 12) & 0x3FFF
+		camera_settings.manual_analog_gain = string.byte(data, 13) & 0xF8
+		camera_settings.manual_red_gain = string.byte(data, 14) << 8 | string.byte(data, 15) & 0x3FF
+		camera_settings.manual_green_gain = string.byte(data, 16) << 8 | string.byte(data, 17) & 0x3FF
+		camera_settings.manual_blue_gain = string.byte(data, 18) << 8 | string.byte(data, 19) & 0x3FF
+	end
+
 	return camera_settings
 end
 
 function _M.camera_capture_and_send(args)
-	quality = args.quality or 50
-	auto_exp_gain_times = args.auto_exp_gain_times or 0
-	metering = args.metering or 'AVERAGE'
-	exposure = args.exposure or 0.18
-	exposure_speed = args.exposure_speed or 0.5
-	shutter_limit = args.shutter_limit or 800
-	analog_gain_limit = args.analog_gain_limit or 248.0
-	white_balance_speed = args.white_balance_speed or 0.5
+	local quality = args.quality or 50
 
-	for run=1,auto_exp_gain_times,1 do
-		frame.camera.auto { metering = metering, exposure = exposure, exposure_speed = exposure_speed, shutter_limit = shutter_limit, analog_gain_limit = analog_gain_limit, white_balance_speed = white_balance_speed }
-		frame.sleep(0.1)
+	if args.auto then
+		local auto_exp_gain_times = args.auto_exp_gain_times or 0
+		local auto_exp_interval = args.auto_exp_interval or 0.1
+		local metering = args.metering or 'AVERAGE'
+		local exposure = args.exposure or 0.18
+		local exposure_speed = args.exposure_speed or 0.5
+		local shutter_limit = args.shutter_limit or 800
+		local analog_gain_limit = args.analog_gain_limit or 248.0
+		local white_balance_speed = args.white_balance_speed or 0.5
+
+		for run=1,auto_exp_gain_times,1 do
+			frame.camera.auto { metering = metering, exposure = exposure, exposure_speed = exposure_speed, shutter_limit = shutter_limit, analog_gain_limit = analog_gain_limit, white_balance_speed = white_balance_speed }
+			frame.sleep(auto_exp_interval)
+		end
+	else
+		local manual_shutter = args.manual_shutter or 800
+		local manual_analog_gain = args.manual_analog_gain or 100
+		local manual_red_gain = args.manual_red_gain or 512
+		local manual_green_gain = args.manual_green_gain or 512
+		local manual_blue_gain = args.manual_blue_gain or 512
+
+		frame.camera.set_shutter(manual_shutter)
+		frame.camera.set_gain(manual_analog_gain)
+		frame.camera.set_white_balance(manual_red_gain, manual_green_gain, manual_blue_gain)
 	end
+
+	-- TODO remove after testing if manual shutter or gain needs a delay to take effect on the first capture after a setting change
+	frame.sleep(0.1)
 
 	frame.camera.capture { quality_factor = quality }
 	-- wait until the capture is finished and the image is ready before continuing
--- lib/tx/camera_settings.dart
+++ lib/tx/camera_settings.dart
@@ -7,33 +7,51 @@ import 'package:simple_frame_app/tx_msg.dart';
 class TxCameraSettings extends TxMsg {
   final int _qualityIndex;
   final int _autoExpGainTimes;
+  final int _autoExpInterval;
   final int _meteringIndex;
   final double _exposure;
   final double _exposureSpeed;
   final int _shutterLimit;
-  final double _whiteBalanceSpeed;
   final int _analogGainLimit;
+  final double _whiteBalanceSpeed;
+  final int _manualShutter;
+  final int _manualAnalogGain;
+  final int _manualRedGain;
+  final int _manualGreenGain;
+  final int _manualBlueGain;
 
   TxCameraSettings({
       required super.msgCode,
       int qualityIndex = 0, // [10, 25, 50, 100];
       int autoExpGainTimes =
-          0, // val >= 0; number of times auto exposure and gain algorithm will be run every 100ms
+          5, // val >= 0; number of times auto exposure and gain algorithm will be run every autoExpInterval ms
+      int autoExpInterval = 100,
       int meteringIndex = 2, // ['SPOT', 'CENTER_WEIGHTED', 'AVERAGE'];
       double exposure = 0.18, // 0.0 <= val <= 1.0
       double exposureSpeed = 0.5, // 0.0 <= val <= 1.0
       int shutterLimit = 800, // 4 <= val <= 16383
       int analogGainLimit = 248, // 0 <= val <= 248
       double whiteBalanceSpeed = 0.5, // 0.0 <= val <= 1.0
+      int manualShutter = 800, // 4 <= val <= 16383
+      int manualAnalogGain = 124, // 0 <= val <= 248
+      int manualRedGain = 512, // 0 <= val <= 1023
+      int manualGreenGain = 512, // 0 <= val <= 1023
+      int manualBlueGain = 512, // 0 <= val <= 1023
       })
       : _qualityIndex = qualityIndex,
         _autoExpGainTimes = autoExpGainTimes,
+        _autoExpInterval = autoExpInterval,
         _meteringIndex = meteringIndex,
         _exposure = exposure,
         _exposureSpeed = exposureSpeed,
         _shutterLimit = shutterLimit,
         _analogGainLimit = analogGainLimit,
-        _whiteBalanceSpeed = whiteBalanceSpeed;
+        _whiteBalanceSpeed = whiteBalanceSpeed,
+        _manualShutter = manualShutter,
+        _manualAnalogGain = manualAnalogGain,
+        _manualRedGain = manualRedGain,
+        _manualGreenGain = manualGreenGain,
+        _manualBlueGain = manualBlueGain;
 
   @override
   Uint8List pack() {
@@ -47,18 +65,42 @@ class TxCameraSettings extends TxMsg {
     int intShutLimMsb = (_shutterLimit >> 8) & 0xFF;
     int intShutLimLsb = _shutterLimit & 0xFF;
 
-    // 9 bytes of camera settings. sendMessage will prepend the data byte, msgCode to each packet
+    // manual shutter has a range 4..16384 so just map it to a Uint16 over 2 bytes
+    int intManShutterMsb = (_manualShutter >> 8) & 0xFF;
+    int intManShutterLsb = _manualShutter & 0xFF;
+
+    // manual color gains have a range 0..1023 so just map them to a Uint16 over 2 bytes
+    int intManRedGainMsb = (_manualRedGain >> 8) & 0x03;
+    int intManRedGainLsb = _manualRedGain & 0xFF;
+    int intManGreenGainMsb = (_manualGreenGain >> 8) & 0x03;
+    int intManGreenGainLsb = _manualGreenGain & 0xFF;
+    int intManBlueGainMsb = (_manualBlueGain >> 8) & 0x03;
+    int intManBlueGainLsb = _manualBlueGain & 0xFF;
+
+
+
+    // 19 bytes of camera settings. sendMessage will prepend the data byte, msgCode to each packet
     // and the Uint16 payload length to the first packet
     return Uint8List.fromList([
       _qualityIndex & 0xFF,
       _autoExpGainTimes & 0xFF,
+      _autoExpInterval & 0xFF,
       _meteringIndex & 0xFF,
       intExp,
       intExpSpeed,
       intShutLimMsb,
       intShutLimLsb,
       _analogGainLimit & 0xFF,
-      intWhiteBalanceSpeed
+      intWhiteBalanceSpeed,
+      intManShutterMsb,
+      intManShutterLsb,
+      _manualAnalogGain & 0xFF,
+      intManRedGainMsb,
+      intManRedGainLsb,
+      intManGreenGainMsb,
+      intManGreenGainLsb,
+      intManBlueGainMsb,
+      intManBlueGainLsb,
     ]);
   }
 }
