bb7b66673af8bce865ddf8c917e2f40b07a585fa
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-11-06 00:16:12 +1100

    enhanced plain_text message packing and parsing to allow x,y position, color, character spacing

--- lib/lua/plain_text.lua
+++ lib/lua/plain_text.lua
@@ -1,10 +1,19 @@
 -- Module to parse text strings sent from phoneside app as TxPlainText messages
 _M = {}
 
+local colors = {'VOID', 'WHITE', 'GRAY', 'RED', 'PINK', 'DARKBROWN','BROWN', 'ORANGE', 'YELLOW', 'DARKGREEN', 'GREEN', 'LIGHTGREEN', 'NIGHTBLUE', 'SEABLUE', 'SKYBLUE', 'CLOUDBLUE'}
+
 -- Parse the TxPlainText message raw data, which is a string
 function _M.parse_plain_text(data)
 	local plain_text = {}
-	plain_text.string = data
+
+	plain_text.x = string.byte(data, 1) << 8 | string.byte(data, 2)
+	plain_text.y = string.byte(data, 3) << 8 | string.byte(data, 4)
+	plain_text.palette_offset = string.byte(data, 5)
+	plain_text.color = colors[plain_text.palette_offset % 16 + 1]
+	plain_text.spacing = string.byte(data, 6)
+	plain_text.string = string.sub(data, 7)
+
 	return plain_text
 end
 
--- lib/tx/plain_text.dart
+++ lib/tx/plain_text.dart
@@ -3,13 +3,32 @@ import 'dart:typed_data';
 
 import 'package:simple_frame_app/tx_msg.dart';
 
-/// A message containing the msgCode and a String of plain text
-/// without any font, formatting or layout information
+/// A message containing the msgCode and a String of plain text,
+/// plus optional top-left corner position coordinates for the
+/// text to be printed in the Frame display (Lua/1-based, i.e. [1,1] to [640,400])
+/// plus an optional palette offset (0..15), plus optional character spacing
 class TxPlainText extends TxMsg {
   final String _text;
+  final int _x, _y;
+  final int _paletteOffset;
+  final int _spacing;
 
-  TxPlainText({required super.msgCode, required String text}) : _text = text;
+  TxPlainText({required super.msgCode, required String text, int x = 1, int y = 1, int paletteOffset = 0, int spacing = 4}) : _text = text, _x = x, _y = y, _paletteOffset = paletteOffset, _spacing = spacing;
 
   @override
-  Uint8List pack() => utf8.encode(_text);
+  Uint8List pack() {
+    final stringBytes = utf8.encode(_text);
+    final strlen = stringBytes.length;
+
+    Uint8List bytes = Uint8List(6 + strlen);
+    bytes[0] = _x >> 8;   // x msb
+    bytes[1] = _x & 0xFF; // x lsb
+    bytes[2] = _y >> 8;   // y msb
+    bytes[3] = _y & 0xFF; // y lsb
+    bytes[4] = _paletteOffset & 0x0F; // 0..15
+    bytes[5] = _spacing & 0xFF;
+    bytes.setRange(6, strlen + 6, stringBytes);
+
+    return bytes;
+  }
 }
