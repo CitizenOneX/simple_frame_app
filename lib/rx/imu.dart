01debb64e5655a20fe9c92eb3298af3d8f9b9502
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-11-08 14:00:03 +1100

    added simple moving average smoothing option for IMU data

--- lib/rx/imu.dart
+++ lib/rx/imu.dart
@@ -6,6 +6,37 @@ import 'package:logging/logging.dart';
 
 final _log = Logger("RxIMU");
 
+/// Buffer class to allow us to provide a smoothed moving average of samples
+class SensorBuffer {
+  final int maxSize;
+  final List<(int x, int y, int z)> _buffer = [];
+
+  SensorBuffer(this.maxSize);
+
+  void add((int x, int y, int z) value) {
+    _buffer.add(value);
+    if (_buffer.length > maxSize) {
+      _buffer.removeAt(0);
+    }
+  }
+
+  (int x, int y, int z) get average {
+    if (_buffer.isEmpty) return (0, 0, 0);
+
+    int sumX = 0, sumY = 0, sumZ = 0;
+    for (var value in _buffer) {
+      sumX += value.$1;
+      sumY += value.$2;
+      sumZ += value.$3;
+    }
+    return (
+      (sumX ~/ _buffer.length),
+      (sumY ~/ _buffer.length),
+      (sumZ ~/ _buffer.length)
+    );
+  }
+}
+
 /// IMU data stream, returns raw 3-axis magnetometer and 3-axis accelerometer data
 /// and optionally computes derived values
 /// Note, a proper calculation of Heading requires magnetometer calibration,
@@ -13,14 +44,23 @@ final _log = Logger("RxIMU");
 /// declination adjustment (which is lat-long and time-dependent).
 /// Magnetometer calibration and declination adjustments need to be done outside this class.
 class RxIMU {
+  final int _smoothingSamples;
 
   // Frame to Phone flags
   final int imuFlag;
   StreamController<IMUData>? _controller;
 
+  // Buffers for smoothing
+  late final SensorBuffer _compassBuffer;
+  late final SensorBuffer _accelBuffer;
+
   RxIMU({
     this.imuFlag = 0x0A,
-  });
+    int smoothingSamples = 1,
+  }) : _smoothingSamples = smoothingSamples {
+    _compassBuffer = SensorBuffer(_smoothingSamples);
+    _accelBuffer = SensorBuffer(_smoothingSamples);
+  }
 
   /// Attach this RxIMU to the Frame's dataResponse characteristic stream.
   Stream<IMUData> attach(Stream<List<int>> dataResponse) {
@@ -41,10 +81,21 @@ class RxIMU {
           // reinterpret the bytes after offset 2 as signed 16-bit integers
           Int16List s16 = bytes.buffer.asInt16List(2);
 
+          // Get raw values
+          var rawCompass = (s16[0], s16[1], s16[2]);
+          var rawAccel = (s16[3], s16[4], s16[5]);
+
+          // Add to buffers
+          _compassBuffer.add(rawCompass);
+          _accelBuffer.add(rawAccel);
+
           _controller!.add(IMUData(
-            // parse out the raw values into (x,y,z) records for compass and accel
-            compass: (s16[0], s16[1], s16[2]),
-            accel: (s16[3], s16[4], s16[5]),
+            compass: _compassBuffer.average,
+            accel: _accelBuffer.average,
+            raw: IMURawData(
+              compass: rawCompass,
+              accel: rawAccel,
+            ),
           ));
 
       }, onDone: _controller!.close, onError: _controller!.addError);
@@ -61,13 +112,25 @@ class RxIMU {
   }
 }
 
-class IMUData {
+class IMURawData {
   final (int x, int y, int z) compass;
   final (int x, int y, int z) accel;
 
+  IMURawData({
+    required this.compass,
+    required this.accel,
+  });
+}
+
+class IMUData {
+  final (int x, int y, int z) compass;
+  final (int x, int y, int z) accel;
+  final IMURawData? raw;
+
   IMUData({
     required this.compass,
-    required this.accel
+    required this.accel,
+    this.raw,
   });
 
   double get pitch => atan2(accel.$2, accel.$3) * 180.0 / pi;
